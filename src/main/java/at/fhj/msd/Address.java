49068681e03f055ec4ab7cd776d39a02a15a96ff
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-31 23:41:21 +0200

    Added new Address.java to rep. Implemented basic Constructor, should replace simple attribut Address in customer.java

---
+++ src/main/java/at/fhj/msd/Address.java
@@ -0,0 +1,23 @@
+package at.fhj.msd;
+
+public class Address {
+
+    private String street;
+    private String place;
+    private String houseNumber;
+    private String zipCode;
+    private String floor;
+    private String doorNumber;
+
+    public Address(String street, String place, String houseNumber, String zipCode, String floor, String doorNumber) {
+        this.street = street;
+        this.place = place;
+        this.houseNumber = houseNumber;
+        this.zipCode = zipCode;
+        this.floor = floor;
+        this.doorNumber = doorNumber;
+    }
+
+    
+    
+}
